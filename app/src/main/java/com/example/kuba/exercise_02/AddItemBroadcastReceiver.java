e3441383ab5a44ef585ae9d5e37dd30b55e40adb
Kuba <jaszczook@gmail.com>
2018-11-17 10:42:25 +0100

    Cleanup

--- app/src/main/java/com/example/kuba/exercise_02/AddItemBroadcastReceiver.java
+++ app/src/main/java/com/example/kuba/exercise_02/AddItemBroadcastReceiver.java
@@ -2,7 +2,6 @@ package com.example.kuba.exercise_02;
 
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
@@ -10,36 +9,39 @@ import android.support.v4.app.NotificationManagerCompat;
 
 public class AddItemBroadcastReceiver extends BroadcastReceiver {
 
-    private static final String ADD_ITEM_CHANNEL = "add_item_channel";
     private int notificationId = 0;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         int id = intent.getIntExtra("id", 0);
         String name = intent.getStringExtra("name");
-        String owner = intent.getStringExtra("owner");
-        createNotification(context, id, name, owner);
+        String username = intent.getStringExtra("username");
+        createNotification(context, id, name, username);
     }
 
-    private void createNotification(Context context, int id, String name, String owner) {
+    private void createNotification(Context context, int id, String name, String username) {
 
-        Intent intentList = new Intent("com.hmkcode.android.another.app.ANOTHER_ACTIVITY");
-        Intent intentEdit = new Intent("com.hmkcode.android.another.app.AND_ANOTHER_ACTIVITY");
+        Intent intentList = new Intent("com.example.kuba.app.activity.ITEM_LIST_ACTIVITY");
+
+        Intent intentEdit = new Intent("com.example.kuba.app.activity.EDIT_ITEM_ACTIVITY");
         intentEdit.putExtra("id", id);
 
         PendingIntent pendingIntentList =
                 PendingIntent.getActivity(context, 0, intentList, 0);
+
         PendingIntent pendingIntentEdit =
                 PendingIntent.getActivity(context, 0, intentEdit, PendingIntent.FLAG_CANCEL_CURRENT);
 
         NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(context)
-                        .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle("ITEM ADDED!")
-                        .addAction(R.drawable.ic_launcher_background, "LOL", pendingIntentList)
-                        .addAction(R.drawable.ic_launcher_background, "LOL2", pendingIntentEdit);
-        if (owner != null && !owner.isEmpty()) {
-            builder.setContentText(name + " added to " + owner + "'s shopping list!");
+                        .setSmallIcon(R.drawable.ic_add_black_24dp)
+                        .addAction(R.drawable.ic_list_black_24dp, "Go to shopping list", pendingIntentList)
+                        .addAction(R.drawable.ic_edit_black_24dp, "Edit item  ", pendingIntentEdit)
+                        .setAutoCancel(true);
+
+        if (username != null && !username.isEmpty()) {
+            builder.setContentText(name + " added to " + username + "'s shopping list!");
         } else {
             builder.setContentText(name + " added to shopping list!");
         }
